4a45bfad2c511d14ce0e468fd0171d4bb5843de5
bernardomft <56193745+bernardomft@users.noreply.github.com>
2021-02-08 11:34:16 +0100

    se dispara kunai y se mueve

--- practica4/practica4.js
+++ practica4/practica4.js
@@ -10,13 +10,18 @@ class NarutoInvaders extends HTMLElement {
         this.render();
         var mapa = this.shadowRoot.querySelector('#mapa');
         var intervalMoveArray = [];
+        var intervalKunai = [];
         //Este evento con su funcion asociado se va a encargar de el movimiento de la nave
         document.addEventListener('keydown', (e) => {
             var nave = this.shadowRoot.querySelector('#nave-principal');
             var step = 15;
             var pos = parseFloat(window.getComputedStyle(nave).bottom.slice(0, -2));
             var pos2 = parseFloat(window.getComputedStyle(nave).left.slice(0, -1));
-            if (e.key === 'ArrowUp') {
+            if(e.key === ' '){
+                console.log('por aqui');
+                this.disparar(intervalKunai);
+            }
+            else if (e.key === 'ArrowUp') {
                 pos = pos + step;
             } else if (e.key === 'ArrowDown') {
                 pos = pos - step;
@@ -48,31 +53,42 @@ class NarutoInvaders extends HTMLElement {
                         ele.setAttribute('style','position:absolute; left: ' + ((i*20)+10)+'%;top:'+pos +'px');
                     else{
                         console.log('game over');
-                        //clearInterval(move);
                         intervalMoveArray.forEach(a => clearInterval(a));
                         clearInterval(interval);
                     }
                 },1000,arrayEnemigos[i],i));
-                /*var move = setInterval(function (ele,i){
-                    var step = 30;
-                    var pos = parseFloat(window.getComputedStyle(ele).top.slice(0, -2));
-                    pos = pos + step;
-                    if(!(pos+80>window.getComputedStyle(mapa).height.slice(0,-2)))
-                        ele.setAttribute('style','position:absolute; left: ' + ((i*20)+10)+'%;top:'+pos +'px');
-                    else{
-                        console.log('game over');
-                        clearInterval(move);
-                        clearInterval(interval);
-                    }
-                },1000,arrayEnemigos[i],i);*/
             }
-
         }, 4000);
         
         
         
     }
-
+    disparar(intervalKunai){
+        var mapa = this.shadowRoot.querySelector('#mapa');
+        var nave = this.shadowRoot.querySelector('#nave-principal');
+        var pos = parseFloat(window.getComputedStyle(nave).bottom.slice(0, -2));
+        var pos2 = parseFloat(window.getComputedStyle(nave).left.slice(0, -1));
+        var rocket = document.createElement('img');
+        rocket.setAttribute('src','img/kunai.png');
+        rocket.setAttribute('height','40');
+        rocket.setAttribute('width','40');
+        rocket.setAttribute('style','position:absolute; bottom:'+ (pos + 80) + 'px; left:' + (pos2 + 20)  + 'px');
+        mapa.appendChild(rocket);
+        intervalKunai.push(setInterval(function (rocket){
+            var step = 5;
+            var pos = parseFloat(window.getComputedStyle(rocket).bottom.slice(0, -2));
+            var pos2 = parseFloat(window.getComputedStyle(rocket).left.slice(0, -2));
+            if(!(pos<0) && !(pos2<=0) && !(pos2>window.getComputedStyle(mapa).width.slice(0,-2)-80)
+                && !(pos>window.getComputedStyle(mapa).height.slice(0,-2))){
+                    rocket.setAttribute('style','position:absolute; left: ' + pos2 + 'px;bottom:'+(pos + step) +'px');
+                }
+            else{
+                console.log('kunai pa cuenca');
+                rocket.setAttribute('style','display:none');
+            }
+           
+        },100,rocket));
+     }
 
 
     render() {
